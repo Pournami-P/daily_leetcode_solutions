01f42cf9752be4ac558ccc6dd5a80825677ec3b4
Pournami-P <pournamip2005@gmail.com>
2025-12-27 11:29:37 +0530

    Add files via upload

---
+++ DSA/Easy/maxAscSubarraySum.java
@@ -0,0 +1,18 @@
+//Day 19
+class  maxAscSubarraySum{
+    public int maxAscendingSum(int[] nums) {
+        int max=nums[0];
+        int currMax=nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(nums[i-1]<nums[i]){
+                currMax=currMax+nums[i];
+            }else{
+                max=Math.max(currMax,max);
+                currMax=nums[i];
+            }
+        }
+        max=Math.max(currMax,max);
+
+        return max;
+    }
+}
